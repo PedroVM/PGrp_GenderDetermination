fd9af5d726e18d3bf858ed5967b2234d9b043edb
Miguel Angel Diaz de la Campa <mdiazcampa@live.com>
2020-05-29 17:13:04 +0200

    soporte para exportar a fichero el tipo de variables

--- helpers/modelFileHelper.py
+++ helpers/modelFileHelper.py
@@ -1,5 +1,5 @@
 import difflib 
-import pandas 
+import pandas as pd
 import numpy as np
 import shared.supportedFiles as const
 
@@ -7,24 +7,24 @@ import shared.supportedFiles as const
 #define a class to help us with the process
 
 class ModelFileHelper(object):
-    """Ayuda a dar una descripcion de un fichero y a su carga """
+    '''Ayuda a dar una descripcion de un fichero y a su carga '''
     def __init__(self, csvFile):
-        self.csvFile= pandas.read_csv(csvFile) 
+        self.__configurePandas()
+        self.csvFile= pd.read_csv(csvFile) 
         self.fileName=csvFile
         self.ColumnDescriptions ={}
-
     def getDescription(self):
         return self.csvFile.describe()
 
     def setColumDescriptionsFile(self, fileFormat, filepath, columnkeyIndex=0, removeNotFoundColumns=True , excelSheet=0):
-        """ vincula las descripciones de las columnas del csv a las descripciones existentes en un fichero adicional.
+        ''' vincula las descripciones de las columnas del csv a las descripciones existentes en un fichero adicional.
         fileFormat, usar la enumeracion SupportedFiles
         filepath es el fichero diccionario.
         columnKeyIndex el índice de columna que contiene el nombre de las columnas.
         removeNotFoundColumns elimina del csv cargado en el helper aquellas columnas no encontradas en el csv diccionario.
         el metodo retorna una lista con las columnas eliminadas cuando removeNotFoundColumns es verdadero.
         el metodo retorna una lista con las columnas No encontradas en el csv de datos cuando removeNotFoundColumns es falso.
-        excelSheet por defecto 0 es la hoja excel de donde queremos cargar los datos """
+        excelSheet por defecto 0 es la hoja excel de donde queremos cargar los datos '''
 
         dataFrame = self.__readFileFormat(filepath,fileFormat,excelSheet)
         #leer y montar en un diccionario key-value donde value es un join del resto de las columnas que no son el columnIndex
@@ -47,15 +47,21 @@ class ModelFileHelper(object):
         return resultado             
 
     def dropColumn(self, columnName):
-        """wrapper para eliminar una columna"""
+        '''wrapper para eliminar una columna'''
         self.csvFile= self.csvFile.drop(columnName, axis=1)
 
-    def getModelTypeDetail(self):
-        """Retorna una estructura legible con los tipos de dato del conjunto de datos del csv cargado"""
-        return self.__translateTypestoHumanReadable(self.csvFile.dtypes)
+    def getModelTypeDetail(self, fileName=None):
+        '''Retorna una estructura legible con los tipos de dato del conjunto de datos del csv cargado
+             exporta la salida a filename si se proporciona un nombre de fichero '''
+        if (fileName==None) :    
+            return self.__translateTypestoHumanReadable(self.csvFile.dtypes)
+        else:
+            with open( fileName, 'w') as f:
+               
+               print(self.__translateTypestoHumanReadable(self.csvFile.dtypes), file=f)     
 
     def findDifferences(self, other):
-        """Retorna una lista con la comparacion de las columnas y los tipos de dos csv"""
+        '''Retorna una lista con la comparacion de las columnas y los tipos de dos csv'''
         returnlist = list (difflib.Differ().compare(self.getModelTypeDetail().to_string().splitlines(1), other.getModelTypeDetail().to_string().splitlines(1)))
         returnlist.append("Comparativa de tamaños: ")
         returnlist.append (self.fileName +  " Filas:" + ''.join(self.__tuplaCleanUp(self.csvFile.shape[0:1])) + " Columnas:" +  ''.join(self.__tuplaCleanUp(self.csvFile.shape[1:2])))
@@ -63,16 +69,16 @@ class ModelFileHelper(object):
         return returnlist
 
     def pearson(self,  A,  B):
-        """ indice de coorrelacion lineal de Pearson de la variable A con respecto a variable B. 
-        Acotado entre [1 , -1] indicando |1| alta coorrelacion y en el caso de ser negativo el coeficiente, correlación inversa """
+        ''' indice de coorrelacion lineal de Pearson de la variable A con respecto a variable B. 
+        Acotado entre [1 , -1] indicando |1| alta coorrelacion y en el caso de ser negativo el coeficiente, correlación inversa '''
         pearson = self.csvFile[A].corr(self.csvFile[B])
         direccion = "directa" if (pearson>0) else ("inversa" if pearson < 0 else "No existe")
         return "Correlación lineal [" + direccion + "]: " + str( abs(pearson) )
 
     def exportHarmonizatedModel(self, harmonizationMatrix, harmonizationquery, fileName):
-        """Exporta el modelo tras armonizar los valores en funcion de una matriz de armonización dada y una query"""
+        '''Exporta el modelo tras armonizar los valores en funcion de una matriz de armonización dada y una query'''
         harmonizated =self.csvFile 
-        dataframe = pandas.DataFrame(harmonizationMatrix)
+        dataframe = pd.DataFrame(harmonizationMatrix)
         for index, trainedRow in dataframe.iterrows() :
             group =harmonizated.query(harmonizationquery) 
             for index, groupRow in  group.iterrows():
@@ -115,10 +121,11 @@ class ModelFileHelper(object):
     
     def __readFileFormat(self, filepath, format, excelSheetIndex=0):
         if (format == const.SupportedFiles.EXCEL):
-            return pandas.read_excel(io= filepath, sheet_name = excelSheetIndex)
+            return pd.read_excel(io= filepath, sheet_name = excelSheetIndex)
         elif (format == const.SupportedFiles.CSV):
-            return pandas.read_csv(filepath)
-      
+            return pd.read_csv(filepath)
         return     
-
+    def __configurePandas(self):
+        pd.set_option('display.max_rows', 3000)
+        pd.set_option('display.max_columns', 3000)
 
